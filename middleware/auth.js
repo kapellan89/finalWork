ddbab5745642b9a95fccc9b0a9e34f1b8d86948f
kapellan89 <serg221089@gmail.com>
2020-03-14 08:54:47 +0300

    commit

--- middleware/auth.js
+++
@@ -1,9 +0,0 @@
-export default function (context) {
-    if (process.server) {
-        if(!context.req.session || !context.req.session.isAuth){
-            return context.redirect('/')
-        }
-    } else if (!context.store.state.authenticated) {
-        return context.redirect('/');
-    }
-}
